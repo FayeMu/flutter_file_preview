870920da87827696288741bf41a36371e8b5959e
Faye <muyf@shinetechchina.com>
2021-12-10 14:29:24 +0800

    feat(iPad): fit

--- ios/Classes/FilePreviewVC.m
+++ ios/Classes/FilePreviewVC.m
@@ -23,9 +23,13 @@
   [backBtn addTarget:self action:@selector(close) forControlEvents:UIControlEventTouchUpInside];
   self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backBtn];
   self.navigationController.navigationBar.backgroundColor = [UIColor whiteColor];
-  self.myWebView = [[WKWebView alloc] initWithFrame: self.view.frame];
-  
-  [self.view addSubview:self.myWebView];
+  self.myWebView = [[WKWebView alloc] init];
+}
+
+- (void)viewDidAppear {
+   [super viewDidAppear];
+   self.myWebView = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+   [self.view addSubview:self.myWebView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
